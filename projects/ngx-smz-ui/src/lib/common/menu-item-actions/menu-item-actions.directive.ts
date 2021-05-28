754d9f3d08e5c69e23923e8973be82984dd042e2
smarza <smarza@gmail.com>
2021-05-28 15:50:23 -0300

    feat: queryParams no menu da ui

--- projects/ngx-smz-ui/src/lib/common/menu-item-actions/menu-item-actions.directive.ts
+++ projects/ngx-smz-ui/src/lib/common/menu-item-actions/menu-item-actions.directive.ts
@@ -16,13 +16,21 @@ export class MenuItemActionsDirective {
     @HostListener('click', ['$event'])
     public onClick(event: any): void {
 
+
         if (this.item.command != null) {
             this.item.command(this.item);
             this.collapse.emit();
         }
         else if (this.item.routerLink != null) {
-            this.store.dispatch(new Navigate(this.item.routerLink));
+
+            if (this.item.queryParams != null) {
+                this.store.dispatch(new Navigate([this.item.routerLink[0], this.item.queryParams]));
+            }
+            else {
+                this.store.dispatch(new Navigate(this.item.routerLink));
+            }
             this.collapse.emit();
+
         }
         else {
             console.error('Nether command nor routerLink was set on the item. Please review the menu item model');
