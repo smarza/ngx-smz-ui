3633d80b836b739b13bce967a398fd777609f0fd
smarza <smarza@gmail.com>
2021-08-06 13:18:53 -0300

    chore: removendo console.log 2.1.45

--- projects/ngx-smz-ui/src/lib/modules/smz-tables/pipes/table-content.pipe.ts
+++ projects/ngx-smz-ui/src/lib/modules/smz-tables/pipes/table-content.pipe.ts
@@ -7,10 +7,7 @@ import { ObjectUtils } from 'primeng/utils';
 
 export class SmzTableContentPipe implements PipeTransform {
   transform(data: any, field: string): { result: string } {
-    console.log(`>> ${field}`, data);
     if (data == null) return { result: '' };
-    console.log(1);
-    console.log(ObjectUtils.resolveFieldData(data, field));
     return { result: ObjectUtils.resolveFieldData(data, field) };
   }
 
