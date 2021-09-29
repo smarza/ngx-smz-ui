44d37b15b981f7c5622f45b7c3f854c7306b74c3
smarza <smarza@gmail.com>
2021-09-29 10:17:16 -0300

    chore: 3.1.14

--- projects/ngx-smz-ui/src/lib/modules/smz-layouts/pages/error/error.component.ts
+++ projects/ngx-smz-ui/src/lib/modules/smz-layouts/pages/error/error.component.ts
@@ -5,6 +5,7 @@ import { FeaturesActions } from '../../../../state/features/features.actions';
 import { GlobalActions } from '../../../../state/global/global.actions';
  import { SmzLayoutsConfig } from '../../core/globals/smz-layouts.config';
 import { RouterDataListenerService } from '../../core/services/router-data-listener.service';
+import { Navigate } from '@ngxs/router-plugin';
 
 @Component({
   selector: 'smz-ui-error',
@@ -14,10 +15,10 @@ import { RouterDataListenerService } from '../../core/services/router-data-liste
 export class ErrorComponent implements OnInit {
 
   constructor(public readonly rbkConfig: NgxRbkUtilsConfig, public readonly config: SmzLayoutsConfig, public readonly routerListener: RouterDataListenerService, private store: Store) {
-    this.clear();
   }
 
   public ngOnInit(): void {
+    this.clear();
   }
 
   public clear(): void {
@@ -25,6 +26,12 @@ export class ErrorComponent implements OnInit {
     this.store.dispatch(new DatabaseActions.Clear());
     this.store.dispatch(new FeaturesActions.Clear());
     localStorage.clear();
+    window.history.pushState('', '', '');
+  }
+
+  public goToLogin(): void {
+    this.store.dispatch(new Navigate([this.rbkConfig.routes.login]));
+    this.clear();
   }
 
 }
