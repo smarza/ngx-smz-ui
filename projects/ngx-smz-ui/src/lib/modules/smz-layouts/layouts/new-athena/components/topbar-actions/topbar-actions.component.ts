8aa085629455d5b9bd509dc7448eb2466ad90f76
rsmarzaro <rsmarzaro@tecgraf.puc-rio.br>
2026-02-09 18:47:04 +0100

    Upgrade Angular and related dependencies to version 21.1.3 in package.json and package-lock.json for improved compatibility. Refactor topbar-actions component to utilize new conditional rendering syntax for better readability.

--- projects/ngx-smz-ui/src/lib/modules/smz-layouts/layouts/new-athena/components/topbar-actions/topbar-actions.component.ts
+++ projects/ngx-smz-ui/src/lib/modules/smz-layouts/layouts/new-athena/components/topbar-actions/topbar-actions.component.ts
@@ -9,62 +9,74 @@ import { GlobalInjector } from '../../../../../../common/services/global-injecto
     styleUrls: ['./topbar-actions.component.scss'],
     template: `
       <ng-content></ng-content>
-
+      
       <div id="smz-ui-topbar"></div>
-
+      
       <smz-responsive class="col grid grid-nogutter w-full items-center justify-start">
-
-      <!-- LANDSCAPE -->
+      
+        <!-- LANDSCAPE -->
         <ng-template pTemplate="landscape">
-
+      
           <span class="col extras-container justify-end extras-container mr-3">
-            <ng-container *ngIf="headerExtrasTemplate != null">
+            @if (headerExtrasTemplate != null) {
               <ng-container *ngTemplateOutlet="headerExtrasTemplate"></ng-container>
-            </ng-container>
+            }
           </span>
-
-          <ng-container *ngIf="uiConfig.rbkUtils.notifications.url != null; else modelDrivenNotificationsTemplate1">
+      
+          @if (uiConfig.rbkUtils.notifications.url != null) {
             <smz-ui-notifications id="smz-ui-notifications" class="mt-2"></smz-ui-notifications>
-          </ng-container>
-
-          <ng-template #modelDrivenNotificationsTemplate1>
-            <span class="notification-container" *ngIf="notifications != null" smz-ui-new-athena-notifications [items]="notifications"></span>
-          </ng-template>
-
-          <smz-localization-switch *ngIf="showLocalizationSwitch" class="mr-3"></smz-localization-switch>
-
-          <smz-tenant-switch *ngIf="showTenantSwitch" class="mr-3"></smz-tenant-switch>
-
-          <span id="smz-ui-profile-menu" *ngIf="profile != null" smz-ui-new-athena-profile-menu [profile]="profile"></span>
-
+          } @else {
+            @if (notifications != null) {
+              <span class="notification-container" smz-ui-new-athena-notifications [items]="notifications"></span>
+            }
+          }
+      
+      
+          @if (showLocalizationSwitch) {
+            <smz-localization-switch class="mr-3"></smz-localization-switch>
+          }
+      
+          @if (showTenantSwitch) {
+            <smz-tenant-switch class="mr-3"></smz-tenant-switch>
+          }
+      
+          @if (profile != null) {
+            <span id="smz-ui-profile-menu" smz-ui-new-athena-profile-menu [profile]="profile"></span>
+          }
+      
         </ng-template>
-
+      
         <!-- PORTRAIT -->
         <ng-template pTemplate="portrait">
-
+      
           <span class="col extras-container justify-end extras-container mr-3">
-            <ng-container *ngIf="headerExtrasTemplate != null">
+            @if (headerExtrasTemplate != null) {
               <ng-container *ngTemplateOutlet="headerExtrasTemplate"></ng-container>
-            </ng-container>
+            }
           </span>
-
-          <ng-container *ngIf="uiConfig.rbkUtils.notifications.url != null; else modelDrivenNotificationsTemplate2">
+      
+          @if (uiConfig.rbkUtils.notifications.url != null) {
             <smz-ui-notifications id="smz-ui-notifications" class="mt-2"></smz-ui-notifications>
-          </ng-container>
-
-          <ng-template #modelDrivenNotificationsTemplate2>
-            <span class="notification-container" *ngIf="notifications != null" smz-ui-new-athena-notifications [items]="notifications"></span>
-          </ng-template>
-
-          <smz-tenant-switch *ngIf="showTenantSwitch" class="mr-3"></smz-tenant-switch>
-
-          <span id="smz-ui-profile-menu" *ngIf="profile != null" smz-ui-new-athena-profile-menu [profile]="profile"></span>
-
+          } @else {
+            @if (notifications != null) {
+              <span class="notification-container" smz-ui-new-athena-notifications [items]="notifications"></span>
+            }
+          }
+      
+      
+          @if (showTenantSwitch) {
+            <smz-tenant-switch class="mr-3"></smz-tenant-switch>
+          }
+      
+          @if (profile != null) {
+            <span id="smz-ui-profile-menu" smz-ui-new-athena-profile-menu [profile]="profile"></span>
+          }
+      
         </ng-template>
-
+      
       </smz-responsive>
-
-  `,
+      
+      `,
     standalone: false
 })
 export class AthenaTopbarActionsComponent {
